3a78f3038b75dd811925384e05271f0d89f58ef5
Aleonard10 <31227161+Aleonard10@users.noreply.github.com>
2018-01-25 14:26:00 -0800

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int value1;
+    int value2;
+
+    cin >> value1;
+    cin >> value2;
+
+    cout << value1 << " + " << value2 << " = " << value1+value2 << endl;
+
+    return 0;
+}
