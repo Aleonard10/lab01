17e765941334949c37b3f2e521bac9e26f9e5cd3
Aleonard10 <31227161+Aleonard10@users.noreply.github.com>
2018-01-25 14:25:06 -0800

    Create print.cpp

---
+++ print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int value1;
+
+    cin >> value1;
+
+    for (int i = 0; i < value1 ; i++)
+    {
+        cout << i << " hello" << endl;
+    }
+    return 0;
+}
